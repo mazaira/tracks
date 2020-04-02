52170ab0c7aeedec533a82dd204129a8a5199b4f
alberto <a.mmazaira@gmail.com>
2020-04-02 13:50:55 +0200

    MVP navigator working with signup and signin

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
+const StackAuth = createStackNavigator();
+const StackTracks = createStackNavigator();
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Auth() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <StackAuth.Navigator>
+      <StackAuth.Screen name="Signup" component={SignupScreen} />
+      <StackAuth.Screen name="Signin" component={SigninScreen} />
+    </StackAuth.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Tracks() {
+  return (
+    <StackTracks.Navigator>
+      <StackTracks.Screen name="TrackList" component={TrackListScreen} />
+      <StackTracks.Screen name="TrackDetail" component={TrackDetailScreen} />
+    </StackTracks.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        { false ? (
+          <>
+            <Tab.Screen name="Account" component={AccountScreen} />
+            <Tab.Screen name="Tracks" component={Tracks} />
+            <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
+          </>
+        ) : (
+            <Stack.Screen name="Auth" component={Auth} />
+          )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default () => {
+  return <App />
+};
